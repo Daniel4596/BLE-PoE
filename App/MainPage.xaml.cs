b317eda72778ad0adba14d8cb1eb0adb6f12f512
Daniel Geermann <120942233+Daniel4596@users.noreply.github.com>
2023-12-21 11:59:33 +0100

    Update MainPage.xaml.cs

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -1,7 +1,6 @@
-﻿using Plugin.BLE;
+using Plugin.BLE;
 using Plugin.BLE.Abstractions;
 using Plugin.BLE.Abstractions.Contracts;
-using Plugin.BLE.Abstractions.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,14 +14,13 @@ namespace BeyondBatteries
     public partial class MainPage : ContentPage
     {
         private readonly IAdapter _bluetoothAdapter;
-        IDevice _connectedDevice;
-        String action;
+        private readonly List<IDevice> _gattDevices = new List<IDevice>();
         public MainPage()
         {
             InitializeComponent();
             _bluetoothAdapter = CrossBluetoothLE.Current.Adapter;
 
-            _bluetoothAdapter.DeviceDiscovered += async (sender, foundBleDevice) =>
+            _bluetoothAdapter.DeviceDiscovered += (sender, foundBleDevice) =>
             {
                  
                 if (foundBleDevice != null && !string.IsNullOrEmpty(foundBleDevice.Device.Name))
@@ -31,28 +29,7 @@ namespace BeyondBatteries
                     {
                         return;
                     }
-                    IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(BLEConnect.IsEnabled = true);
-                    await _bluetoothAdapter.StopScanningForDevicesAsync();
-                    action = await DisplayActionSheet("Choose device", "Cancel", null, foundBleDevice.Device.Name);
-
-                    if (action == foundBleDevice.Device.Name)
-                    {
-                        
-                        _connectedDevice = foundBleDevice.Device;
-                        if (_connectedDevice.State == DeviceState.Connected)
-                        {
-                            await Navigation.PushAsync(new DevicePage(foundBleDevice.Device));
-                        }
-                        else
-                        {
-                            
-                            var connectParameters = new ConnectParameters(false, true);
-                            await _bluetoothAdapter.ConnectToDeviceAsync(_connectedDevice, connectParameters);
-
-                            await Navigation.PushAsync(new DevicePage(_connectedDevice));
-                        }
-
-                    }
+                    _gattDevices.Add(foundBleDevice.Device);
                 }
             
             };
@@ -98,23 +75,52 @@ namespace BeyondBatteries
         private async void OnPress(object sender, EventArgs e)
         {
             IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(BLEConnect.IsEnabled = false);
+            foundBleDevicesListView.ItemsSource = null;
             if (!await PermissionGrantedAsync())                                                           // Make sure there is permission to use Bluetooth
             {
                 await DisplayAlert("Permission required", "Application needs location permission." + "Permissions needs to be set manually!", "OK");
                 return;
             }
 
+            _gattDevices.Clear();
 
             if(!_bluetoothAdapter.IsScanning)
             {
                 await _bluetoothAdapter.StartScanningForDevicesAsync();
             }
             
+            foreach (var device in _bluetoothAdapter.ConnectedDevices)
+                _gattDevices.Add(device);
+
+            foundBleDevicesListView.ItemsSource = _gattDevices.ToArray();
             IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(BLEConnect.IsEnabled = true);
 
         }
 
+        private async void FoundBluetoothDevicesListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(BLEConnect.IsEnabled = false);
+            IDevice selectedItem = e.Item as IDevice;
 
+            if (selectedItem.State == DeviceState.Connected)
+            {
+                await Navigation.PushAsync(new DevicePage(selectedItem));
+            }
+            else
+            {
+                try
+                {
+                    var connectParameters = new ConnectParameters(false, true);
+                    await _bluetoothAdapter.ConnectToDeviceAsync(selectedItem, connectParameters);
+                    await Navigation.PushAsync(new DevicePage(selectedItem));
+                }
+                catch
+                {
+                    await DisplayAlert("Error connecting", $"Error connecting to BLE device: {selectedItem.Name}", "OK");
+                }
+            }
+            IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(BLEConnect.IsEnabled = true);
+        }
 
     }
 }
